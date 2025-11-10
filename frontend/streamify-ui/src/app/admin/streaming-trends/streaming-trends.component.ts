1398bef6d3e7ada1e93fcf949b45e818beeab47e
Portelin1 <owenporter04@gmail.com>
2025-11-09 20:15:02 -0500

    be sure to do npm install chart.js for the chart builder to work

--- frontend/streamify-ui/src/app/admin/streaming-trends/streaming-trends.component.ts
+++ frontend/streamify-ui/src/app/admin/streaming-trends/streaming-trends.component.ts
@@ -1,11 +1,57 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Chart } from 'chart.js/auto';
+import {CommonModule} from '@angular/common';
+import {FormsModule} from '@angular/forms';
 
 @Component({
   selector: 'app-streaming-trends',
   standalone: true,
-  imports: [],
-  template: '<p>streaming-trends works!</p>',
+  imports: [CommonModule, FormsModule],
+  templateUrl: './streaming-trends.component.html',
+  styleUrls: ['./streaming-trends.component.scss'],
 })
-export class StreamingTrendsComponent {
+
+export class StreamingTrendsComponent implements OnInit {
+
+  apiUrl = "/api/content"
+  trends: any[] = [];
+
+  constructor(private http: HttpClient) { }
+
+  ngOnInit(): void {
+    this.loadTrends
+  }
+
+  loadTrends(){
+    this.http.get<any[]>(`${this.apiUrl}/last-24h-trends`)
+      .subscribe(data =>{
+        this.trends = data;
+        console.log(this.trends);
+        this.buildChart();
+      })
+  }
+
+  buildChart() {
+    const labels = this.trends.map(item => item.title);
+    const dataPoints = this.trends.map(item => item.stream_count);
+
+    new Chart("trends",{
+      type: 'bar',
+      data: {
+        labels: labels,
+        datasets: [{
+          label: "Streams in the last 24 hours",
+          data: dataPoints,
+          borderWidth: 1
+        }]
+      },
+      options: {
+        scales: {
+          y: {beginAtZero: true},
+        }
+      }
+    });
+  }
 
 }
