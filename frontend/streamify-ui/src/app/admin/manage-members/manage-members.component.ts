0cf1ae0c2a3a710b80c1d922d47ae67ec37613e7
Portelin1 <owenporter04@gmail.com>
2025-12-03 10:52:36 -0500

    delete and add member on admin side done, just needs to look pretty

--- frontend/streamify-ui/src/app/admin/manage-members/manage-members.component.ts
+++ frontend/streamify-ui/src/app/admin/manage-members/manage-members.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit, ChangeDetectorRef } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
 
 export interface Member {
   email: string;
@@ -12,13 +13,27 @@ export interface Member {
 @Component({
   selector: 'app-manage-members',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, FormsModule],
   templateUrl: './manage-members.component.html',
   styleUrls: ['./manage-members.component.scss']
 })
 export class ManageMembersComponent implements OnInit {
   members: Member[] = [];
   apiUrl = "/api/content"
+
+  showAddForm = false;
+
+  newMember = {
+    email: "",
+    password: "",
+    name: "",
+    street: "",
+    city: "",
+    state: "",
+    country: "",
+    phone: "",
+    subName: ""
+  }
 
   constructor(private http: HttpClient, private cdr: ChangeDetectorRef) { }
 
@@ -35,10 +50,52 @@ export class ManageMembersComponent implements OnInit {
   }
 
   addMember(): void {
-    console.log('Add member clicked');
+    this.showAddForm = true;
+    this.newMember = {
+      email: '',
+      password: '',
+      name: '',
+      street: '',
+      city: '',
+      state: '',
+      country: '',
+      phone: '',
+      subName: ''
+    }
+  }
+
+  submitNewMember(): void {
+    this.http.post(`${this.apiUrl}/member`, this.newMember, {
+      responseType: 'text' as 'json'
+    }).subscribe({next: () => {
+      this.fetchMembers();
+      this.showAddForm = false;},
+    error: (err) => {
+      console.log("failed to add member", err);}
+    });
+  }
+
+  cancelNewMember(): void {
+    this.showAddForm = false;
   }
 
   removeMember(member: Member): void {
-    console.log('Remove member:', member);
+    const body={
+      email: member.email,
+      member: member.member_id
+    };
+
+    this.http.delete(`${this.apiUrl}/member`, {
+      body,
+      responseType: 'text'
+    }).subscribe({next: () =>{
+      this.members = this.members.filter(m =>
+        !(m.member_id === member.member_id && m.email === member.email));
+      this.cdr.detectChanges();
+      },
+    error: (err) => {
+      console.error("failed to delete member", err);
+    }
+    });
   }
 }
